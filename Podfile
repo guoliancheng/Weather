379641006b7d97dbc44789f6d5d1989f9de379c8
郭连城 <guo_136@sina.cn>
2018-11-21 15:23:40 +0800

    [feature] 建立工程

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '8.0'
+use_frameworks!
+
+
+target 'Weather' do
+pod 'SnapKit', '~> 4.0.0'
+#pod 'SVProgressHUD', '~> 2.2.5'
+#pod 'iOSDFULibrary', '~> 4.1.3'
+#pod 'Bugly'
+pod 'Alamofire', '~> 4.7.2'
+# pod 'EVReflection', '~> 5.0'
+# pod 'SWRevealViewController', '~> 2.3'
+# pod 'CorePlot', '~> 2.2'
+end
+
---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 郭连城 on 2018/11/21.
+//  Copyright © 2018 郭连城. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
